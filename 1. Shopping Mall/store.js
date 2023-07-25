e820ad58f441600f04475597d4d2c9fca3708481
김모남 <138136517+monam2@users.noreply.github.com>
2023-07-25 18:18:02 +0900

    js파일 생성

---
+++ 1. Shopping Mall/store.js
@@ -0,0 +1,12 @@
+let templete = `<div class="card">
+<img src="./mall_images/pr1.JPG" class="w-100" alt="" />
+<h5>title</h5>
+<p class="card-explain">상품 설명</p>
+<p class="card-price">가격 : 30000</p>
+<div class="btn-list">
+  <button class="btn btn-light">구매</button>
+  <button class="btn btn-light">취소</button>
+</div>
+</div>`
+
+$(".container-templete").append(templete);
