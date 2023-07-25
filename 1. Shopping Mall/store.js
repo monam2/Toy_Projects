c035c8c67763449e26907e8a2967b3df54048caa
김모남 <138136517+monam2@users.noreply.github.com>
2023-07-25 20:04:54 +0900

    Update store.js

--- 1. Shopping Mall/store.js
+++ 1. Shopping Mall/store.js
@@ -1,12 +1,29 @@
-let templete = `<div class="card">
-<img src="./mall_images/pr1.JPG" class="w-100" alt="" />
-<h5>title</h5>
-<p class="card-explain">상품 설명</p>
-<p class="card-price">가격 : 30000</p>
+$.get("./store.json").done((jsondata) => {
+  products = jsondata.products;
+  products.forEach((v) => {
+    makeTemplete(v)
+  });
+
+
+
+  $(".buy").click(function (e) {
+    let title = e.target.parentNode.previousElementSibling.previousElementSibling.previousElementSibling.innerHTML
+    let price = e.target.parentNode.previousElementSibling.innerHTML.replace(/[^0-9]/g, ' ')
+    console.log(title, price)
+  })
+})
+
+const makeTemplete = ((data) => {
+  let templete = `<div class="card">
+<img src="./mall_images/${data.photo}" class="w-100" alt="" />
+<h5>${data.title}</h5>
+<p class="card-brand">${data.brand}</p>
+<p class="card-price">가격 : ${data.price}</p>
 <div class="btn-list">
-  <button class="btn btn-light">구매</button>
-  <button class="btn btn-light">취소</button>
+  <button class="btn btn-light buy">구매</button>
+  <button class="btn btn-light buyCancel">취소</button>
 </div>
 </div>`
+  $(".container-templete").append(templete);
+})
 
-$(".container-templete").append(templete);
