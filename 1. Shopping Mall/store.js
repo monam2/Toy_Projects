8e9b68f244a156b242e262d5e13540f73a44b1b4
김모남 <138136517+monam2@users.noreply.github.com>
2023-07-26 16:11:33 +0900

    Update store.js
    검색 기능 구현.
    - 검색 버튼을 누르면 input창에 있는 내용을 받아와 처리함.
    - 검색어가 없을 경우 '검색어가 없습니다!' alert 출력 후 상품 목록 유지
    - 올바른 검색어가 아닐 경우 '올바른 검색어를 입력하세요!' alert 출력 후 상품 목록 유지
    - 올바른 검색어일 경우 해당 상품만 목록에 보여줌. 검색어가 여러 상품에 있을 경우 모두 보여줌.
    
    고찰
    - 기능 구현 자체는 크게 어렵지 않았던 것 같은데, 여러 상품에 같은 검색어가 있을 경우 제일 첫 번째 상품만 나오고, 나머지는 나오지 않는 문제가 있었다.
      처음엔 불리언 타입의 변수를 둬서 실행 이전에 false로 둔 뒤, 첫 실행시에만 true로 변경하여 이후에 나머지도 출력하도록 하려했으나 문제는 이 부분이 아니었다.
    - 검색버튼을 클릭한 후의 반복문이 문제였다. for (let i = 0; i < products.length; i++) 반복적으로 카드를 생성하기 위한 구문인데, 초기에 length를 products(json자료)가 아닌 카드의 갯수만큼 줬기 때문에, $(".container-templete").html(''); 로 인해서 카드의 내용이 아예 비워진 상태에서 시작할 때는 반복문이 동작하지 않았고, 위의 비우는 코드의 위치를 바꾸더라도 카드 하나를 만들고 초기화가 되기 때문에 한 장의 카드만이 출력되는 것이었다.
    - 고찰을 적으면서 느끼는데 내가 짠 코드더라도 설명하는게 참 어렵다는 것을 새삼 느꼈다. 코드를 작성/수정하면서 이 코드가 무엇이고 어떤 동작을 하는지, 어떤 변수(함수)와 연결되는지를 상세히 기록해 놓아야 다른 사람이 내 코드를 읽었을 때 이해할 수 있을 것 같다.

--- 1. Shopping Mall/store.js
+++ 1. Shopping Mall/store.js
@@ -4,13 +4,47 @@ $.get("./store.json").done((jsondata) => {
     makeTemplete(v)
   });
 
-
-
   $(".buy").click(function (e) {
     let title = e.target.parentNode.previousElementSibling.previousElementSibling.previousElementSibling.innerHTML
-    let price = e.target.parentNode.previousElementSibling.innerHTML.replace(/[^0-9]/g, ' ')
+    let price = e.target.parentNode.previousElementSibling.innerHTML.replace(/[^0-9]/g, '')
     console.log(title, price)
   })
+
+  $(".searchButton").on('click', function (e) {
+    let words = e.target.previousElementSibling.value
+    if (words != false) {
+      $(".container-templete").html('');
+
+      for (let i = 0; i < products.length; i++) {
+        if (products[i].title.includes(words) || products[i].brand.includes(words)) {
+          let templete = `<div class="card">
+<img src="./mall_images/${products[i].photo}" class="w-100" alt="" />
+<h5>${products[i].title}</h5>
+<p class="card-brand">${products[i].brand}</p>
+<p class="card-price">가격 : ${products[i].price}</p>
+<div class="btn-list">
+  <button class="btn btn-light buy">구매</button>
+  <button class="btn btn-light buyCancel">취소</button>
+</div>
+</div>`
+          $(".container-templete").append(templete);
+        } else {
+          alert('올바른 검색어를 입력하세요!')
+          products.forEach((v) => {
+            makeTemplete(v)
+          });
+          return
+        }
+      }
+    } else {
+      alert('검색어가 없습니다!');
+      products.forEach((v) => {
+        makeTemplete(v)
+      });
+    }
+  })
+
+
 })
 
 const makeTemplete = ((data) => {
